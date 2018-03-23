85f067381fd41a94dd9be48121dd9d6c698c8dc1
Boris <boris.misnik@corp.hitwe.com>
2018-03-23 12:55:25 +0200

    fix for android oreo

--- android/src/main/java/io/invertase/firebase/messaging/RNFirebaseBackgroundMessagingService.java
+++ android/src/main/java/io/invertase/firebase/messaging/RNFirebaseBackgroundMessagingService.java
@@ -1,10 +1,12 @@
 package io.invertase.firebase.messaging;
 
+import android.app.Notification;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
 
 import com.facebook.react.HeadlessJsTaskService;
-import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.jstasks.HeadlessJsTaskConfig;
 import com.google.firebase.messaging.RemoteMessage;
@@ -12,19 +14,34 @@ import com.google.firebase.messaging.RemoteMessage;
 import javax.annotation.Nullable;
 
 public class RNFirebaseBackgroundMessagingService extends HeadlessJsTaskService {
-  @Override
-  protected @Nullable HeadlessJsTaskConfig getTaskConfig(Intent intent) {
-    Bundle extras = intent.getExtras();
-    if (extras != null) {
-      RemoteMessage message = intent.getParcelableExtra("message");
-      WritableMap messageMap = MessagingSerializer.parseRemoteMessage(message);
-      return new HeadlessJsTaskConfig(
-        "RNFirebaseBackgroundMessage",
-        messageMap,
-        60000,
-        false
-      );
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            Notification notification = new NotificationCompat.Builder(this, "taimi_all")
+                    .setContentTitle("Taimi")
+                    .setContentText("New notification").build();
+
+            startForeground(1337, notification);
+            stopForeground(true);
+        }
     }
-    return null;
-  }
+
+    @Override
+    protected @Nullable
+    HeadlessJsTaskConfig getTaskConfig(Intent intent) {
+        Bundle extras = intent.getExtras();
+        if (extras != null) {
+            RemoteMessage message = intent.getParcelableExtra("message");
+            WritableMap messageMap = MessagingSerializer.parseRemoteMessage(message);
+            return new HeadlessJsTaskConfig(
+                    "RNFirebaseBackgroundMessage",
+                    messageMap,
+                    60000,
+                    false
+            );
+        }
+        return null;
+    }
 }
